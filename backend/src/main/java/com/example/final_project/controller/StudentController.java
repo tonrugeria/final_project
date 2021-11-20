ebea4d757d709bb8ef0b19a3fc3b1027e43828ff
Kim Naguit <kimcrismelnaguit05@gmail.com>
2021-11-20 21:41:35 +0800

    added studentcontroller and test the get mapping
    added studentcontroller and test the get mapping

---
+++ backend/src/main/java/com/example/final_project/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.final_project.controller;
+
+import com.example.final_project.exception.ResourceNotFoundException;
+import com.example.final_project.model.Student;
+import com.example.final_project.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:3000/"})
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+}
